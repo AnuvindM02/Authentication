198117f2a32ae41ae9a7ada2dc082ee9638fd51e
Anuvind <anuvindm02@gmail,com>
2025-04-03 00:05:07 +0530

    new endpoint for JWKS

---
+++ Authentication.Application/Features/GetJWKS/GetJWKSResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Authentication.Application.Features.GetJWKS
+{
+    public class GetJWKSResponse
+    {
+        public required string Kty {  get; set; }
+        public required string Use { get; set; }
+        public required string Kid { get; set; }
+        public required string Alg {  get; set; }
+        public required string N {  get; set; }
+        public required string E { get; set; }
+    }
+}
--- Authentication.Infrastructure/Repositories/SigningKeyRepository.cs
+++ Authentication.Infrastructure/Repositories/SigningKeyRepository.cs
@@ -1,21 +1,21 @@
-﻿using Authentication.Application.Interfaces.Repositories;
+﻿using System.Linq.Expressions;
+using Authentication.Application.Interfaces.Repositories;
 using Authentication.Domain.Entities;
 using Authentication.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 namespace Authentication.Infrastructure.Repositories
 {
-    public class SigningKeyRepository : ISigningKeyRepository
+    public class SigningKeyRepository(ApplicationDbContext _context) : ISigningKeyRepository
     {
-        private readonly ApplicationDbContext _context;
-        public SigningKeyRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
         public async Task<SigningKey?> GetActiveSigningKey(CancellationToken cancellationToken)
         {
             return await _context.SigningKeys.FirstOrDefaultAsync(k => k.IsActive, cancellationToken);
+        }
+
+        public async Task<List<SigningKey>?> GetAllByCondition(Expression<Func<SigningKey, bool>> predicate, CancellationToken cancellationToken)
+        {
+            return await _context.SigningKeys.Where(predicate).ToListAsync(cancellationToken);
         }
     }
 }
